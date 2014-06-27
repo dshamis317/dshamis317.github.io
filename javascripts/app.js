fd8d8f76312147e7062f61e4990dafb270dda9f7
dshamis317 <dshamis317@gmail.com>
2014-06-27 16:53:26 -0400

    sliding page

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,3 +1,22 @@
 $(function () {
+  // var router = new Router();
+  // Backbone.history.start();
 
+  $('#about').click(function() {
+      $('html, body').animate({
+        scrollTop: $(".about-section").offset().top
+      }, 100);
+    })
+
+  $('#portfolio').click(function() {
+      $('html, body').animate({
+        scrollTop: $(".portfolio-section").offset().top
+      }, 100);
+    })
+
+  $('#contact').click(function() {
+      $('html, body').animate({
+        scrollTop: $(".contact-section").offset().top
+      }, 100);
+    })
 })
---
+++ javascripts/backbone/router.js
@@ -0,0 +1,25 @@
+// Router = Backbone.Router.extend({
+//   routes: {
+//     ''         : 'index',
+//     'about'    : 'aboutMe',
+//     'portfolio': 'myPortfolio',
+//     'contact'  : 'contactMe'
+//   },
+//   index: function() {
+//     console.log('Thank you for visiting my website. Hope to be in touch soon! Best, Dmitry')
+//   },
+//   aboutMe: function() {
+//     $('#about').click(function() {
+//       $('html, body').animate({
+//         scrollTop: $(".about-section").offset().top
+//       }, 100);
+//     })
+//   },
+//   myPortfolio: function() {
+//     $('#portfolio').click(function() {
+//       $('html, body').animate({
+//         scrollTop: $(".portfolio-section").offset().top
+//       }, 100);
+//     })
+//   },
+// })
